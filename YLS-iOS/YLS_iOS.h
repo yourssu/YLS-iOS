33b4b7cd4a010ef93b5f4912e7192b8b2c5fa960
정지혁 <cars0106@icloud.com>
2024-01-30 01:55:39 +0900

    [#1] 빈 프로젝트 생성 및 .gitignore 파일 생성

---
+++ YLS-iOS/YLS_iOS.h
@@ -0,0 +1,18 @@
+//
+//  YLS_iOS.h
+//  YLS-iOS
+//
+//  Created by 정지혁 on 1/30/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YLS_iOS.
+FOUNDATION_EXPORT double YLS_iOSVersionNumber;
+
+//! Project version string for YLS_iOS.
+FOUNDATION_EXPORT const unsigned char YLS_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YLS_iOS/PublicHeader.h>
+
+
