54221d35666197ed766ebd358a6fe6fc2fa2ba1a
정지혁 <cars0106@icloud.com>
2024-02-16 12:11:02 +0900

    [#9] 변환된 문자열 테스트 코드 작성

--- Tests/YLS-iOSTests/YLS_iOSTests.swift
+++ Tests/YLS-iOSTests/YLS_iOSTests.swift
@@ -8,5 +8,14 @@ final class YLS_iOSTests: XCTestCase {
 
         // Defining Test Cases and Test Methods
         // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+        let shared = YLS.shared
+
+        for _ in 1...100 {
+            let random = shared.fetchRandomString(length: 10)
+            let randomHashed = shared.hashUserID(userID: random)
+            let data = randomHashed.data(using: .utf8)!
+            let byteCount = data.count
+            XCTAssertEqual(byteCount, 64)
+        }
     }
 }
